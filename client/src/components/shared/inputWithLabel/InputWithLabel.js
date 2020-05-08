cfa085ae40c829774f9f4c94b959ce58353d3ba9
Joanna Chądzyńska <jmalek87@gmail.com>
2020-05-08 19:12:21 +0200

    Created reusable input with label component

---
+++ client/src/components/shared/inputWithLabel/InputWithLabel.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Register from "../../auth/+Register/Register";
+
+const InputWithLabel = ({ children, onInputChange, value, ...props }) => {
+	const { type } = props;
+	return (
+		<div className='form-group'>
+			<label>{children}</label>
+			<input onChange={onInputChange} value={value} {...props} />
+			{type === "email" && (
+				<small className='form-text'>
+					This site uses Gravatar so if you want a profile image, use a Gravatar
+					email
+				</small>
+			)}
+		</div>
+	);
+};
+
+export default InputWithLabel;
