edc11ccd06f99ec596940b727ad39d6bf99f86d5
Joanna Chądzyńska <jmalek87@gmail.com>
2020-05-11 21:54:53 +0200

    Added profile about and updated profile top

--- client/src/components/profile/components/ProfileTop.js
+++ client/src/components/profile/components/ProfileTop.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const ProfileTop = ({
 	profile: { status, company, location, website, social, user },
@@ -48,6 +48,10 @@ const ProfileTop = ({
 			</div>
 		</div>
 	);
+};
+
+ProfileTop.propTypes = {
+	profile: PropTypes.object.isRequired,
 };
 
 export default ProfileTop;
