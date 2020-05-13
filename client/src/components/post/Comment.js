873a8d258468fb83772279927504cacc7970bf0e
Joanna Chądzyńska <jmalek87@gmail.com>
2020-05-13 18:31:14 +0200

    Display comments in post component

---
+++ client/src/components/post/Comment.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from "react-redux";
+import Moment from "react-moment";
+import { deleteComment } from "../../redux/actions/post.actions";
+
+const Comment = ({
+	comment: { _id, text, name, user, avatar, date },
+	postId,
+}) => {
+	const dispatch = useDispatch();
+	const auth = useSelector((state) => state.auth);
+
+	return (
+		<div className='post bg-white p-1 my-1'>
+			<div>
+				<Link to={`/profile/${user}`}>
+					<img className='round-img' src={avatar} alt='' />
+					<h4>{name}</h4>
+				</Link>
+			</div>
+			<div>
+				<p className='my-1'>{text}</p>
+				<p className='post-date'>
+					Posted on <Moment form='YYYY/MM/DD'>{date}</Moment>
+				</p>
+			</div>
+		</div>
+	);
+};
+
+Comment.propTypes = {
+	postId: PropTypes.number.isRequired,
+	comment: PropTypes.object.isRequired,
+};
+
+export default Comment;
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { getPost } from "../../redux/actions/post.actions";
 import { Spinner } from "../layout";
 import PostItem from "../posts/PostItem";
 import CommentForm from "./CommentForm";
+import Comment from "./Comment";
 
 const Post = ({ match }) => {
 	const posts = useSelector((state) => state.post);
@@ -23,8 +24,14 @@ const Post = ({ match }) => {
 				Back To Posts
 			</Link>
 			<PostItem post={post} showActions={false} />
+			<div className='comments'>
+				{post.comments.map((comment) => (
+					<Comment key={comment._id} comment={comment} postId={post._id} />
+				))}
+			</div>
 			<CommentForm postId={post._id} />
 		</>
 	);
 };
+
 export default Post;
